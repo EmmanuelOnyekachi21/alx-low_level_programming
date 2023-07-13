ff5132d649494f569b31f00543fc127b155e5baf
Emmanuel <emmanuelonyekachi04122000@gmail.com>
2023-07-13 17:38:39 +0000

    Task 4 - Added description, return value description of the function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,11 @@
 /**
  * main - Entry point
  *
- * return: 0
+ * Description: Prints statement to output.
+ * Return: 0 if successful
  */
 
-int main()
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
