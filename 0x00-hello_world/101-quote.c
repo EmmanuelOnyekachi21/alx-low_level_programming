c7ade3d3957b727ce40f551a7ff51cf981201893
Emmanuel <emmanuelonyekachi04122000@gmail.com>
2023-07-13 18:37:44 +0000

    Advanced Task 2

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - ENtry point
+ * Description: Prints to output without using funx in the NAME section
+ * Return: 1 
+ */
+
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+	return (1);
+}
+		
