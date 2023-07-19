f7498de28af215b0a0a0f756559c66d2d54f12c0
Emmanuel <emmanuelonyekachi04122000@gmail.com>
2023-07-19 18:36:45 +0000

    Task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,13 +11,19 @@
  */
 int print_last_digit(int n)
 {
-	int l;
+	int l, r;
 
 	if (n < 0)
-		n = -1 * n;
-
-	l = n % 10;
-
-	_putchar(l + '0');
-	return (l);
+	{
+		r = -1 * n;
+		l = r % 10;
+		_putchar(l + '0');
+		return (l);
+	}
+	else
+	{
+		l = n % 10;
+		_putchar(l + '0');
+		return (l);
+	}
 }
