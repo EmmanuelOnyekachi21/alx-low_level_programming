de1e6dde307b742c50fcff74bf7bc831d4e6cfdd
Emmanuel <emmanuelonyekachi04122000@gmail.com>
2023-07-19 13:23:29 +0000

    Task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * int _isalpha - Entry point
+ *
+ * @c: Charcter to be procesed
+ * Description: checks for alphabetic character.
+ *
+ * Return: Returns 1 if c is a letter, lowercase or uppercase.
+ */
+
+int _isalpha(int c)
+{
+	int counter1, counter2;
+
+	for (counter1 = 65; counter1 <= 90; counter1++)
+	{
+		if (c == counter1)
+			return (1);
+	}
+
+	for (counter2 = 97; counter2 <= 122; counter2++)
+	{
+		if (c == counter2)
+			return (1);
+	}
+
+	return (0);
+}
