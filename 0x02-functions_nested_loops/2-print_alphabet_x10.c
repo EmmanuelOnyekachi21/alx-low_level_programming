263e90ce1278413643177af96ba9e519bd674a5f
Emmanuel <emmanuelonyekachi04122000@gmail.com>
2023-07-19 04:44:49 +0000

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: prints lowercase 10 times
+ *
+ * Return: nothing
+ */
+
+void print_alphabet_x10(void)
+{
+	int i; /* Counter */
+	char letters;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (letters = 'a'; letters <= 'z'; letters++)
+		{
+			_putchar(letters);
+		}
+		_putchar('\n');
+	}
+}
