a592177323be5ab266e4e160aecddf5246922375
Emmanuel <emmanuelonyekachi04122000@gmail.com>
2023-07-22 06:02:50 +0000

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,16 +12,20 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
-			printf("Fizz");
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
 		else if (i % 5 == 0)
 			printf("Buzz");
-		else if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
 		else
 			printf("%d", i);
+		if (i < 100)
+			printf(" ");
 	}
+
+	printf("\n");
 	return (0);
 }
