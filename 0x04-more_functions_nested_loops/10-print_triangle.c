c00a84200718b9a0fad197162579b67ec1e88052
Emmanuel <emmanuelonyekachi04122000@gmail.com>
2023-07-22 08:30:38 +0000

    Task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,22 +1,36 @@
+#include <stdio.h>
 #include "main.h"
-
 /**
- * print_triangle - prints a triangle.
- * @size: numbers of lines.
- * Return: no return.
+ * print_triangle - Entry point
+ *
+ * @size: The size of the triangle
+ *
+ * Description: prints a triangle
+ *
+ * Return: Returns nothing
  */
+
 void print_triangle(int size)
 {
-	int i, j;
+	int i, j, spaces;
 
-	for (i = 0; i < size; i++)
+	if (size <= 0)
 	{
-		for (j = 1; j < (size - i); j++)
-			_putchar(' ');
-		for (j--; j < size; j++)
-			_putchar(35);
-		if (i < (size - 1))
-			_putchar('\n');
+		_putchar('\n');
+		return;
 	}
-	_putchar('\n');
+
+	for (i = 1; i <= size; i++)
+	{
+		/* Calculate the number of spaces needed in each row */
+		spaces = size - i;
+
+		/* Print the spaces before the '#' characters */
+		for (j = 1; j <= spaces; j++)
+			_putchar(' ');
+
+		for (j = 1; j <= i; j++)
+			_putchar('#');
+		_putchar('\n');
+	}
 }
