74820d9706145c142400eb37017fa3b5a392c1d3
Emmanuel <emmanuelonyekachi04122000@gmail.com>
2023-07-26 02:30:06 +0000

    Task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * puts_half - Entry point
+ *
+ * @str: String to be manipulated.
+ *
+ * Description: prints half of a string, followed by a new line.
+ *
+ * Return: Return nothing
+ */
+
+void puts_half(char *str)
+{
+	int counter = 0, n;
+	char *start = str;
+
+	while (*str++)
+		counter++;
+	str = start;
+
+	if (counter % 2 != 0)
+		n = (counter - 1) / 2;
+	else
+		n = counter / 2;
+
+	while (*(str + n) != '\0')
+	{
+		_putchar(*(str + n));
+		n++;
+	}
+	_putchar('\n');
+}
