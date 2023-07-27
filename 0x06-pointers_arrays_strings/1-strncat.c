f61b495ef2475e69e029078feb719b87358d0637
Emmanuel <emmanuelonyekachi04122000@gmail.com>
2023-07-27 00:00:09 +0000

    Task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * *_strncat - Entry point
+ *
+ * @dest: Destination
+ * @src: Source
+ * Description: concatenates two strings, it will use at most n bytes from src.
+ *
+ * Return: Return dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int length = 0, x;
+	char *str = dest;
+
+	while (*dest++)
+		length++;
+
+	/* Reset the 'dest' pointer to the beginning of the string */
+	dest = str;
+
+	x = 0;
+	while (x < n)
+	{
+		*(dest + length + x) = *(src + x);
+		/* Stop copying if null character is encountered */
+		if (src[x] == '\0')
+			break;
+		x++;
+	}
+
+	*(dest + length + x) = '\0';
+
+	return (dest);
+}
