7ed006b24c2101396749a9edc121bf2d525d4e6b
Emmanuel <emmanuelonyekachi04122000@gmail.com>
2023-07-27 00:35:51 +0000

    Task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * *_strncpy - Entry point
+ *
+ * @dest: Destination
+ * @src: Source
+ * @n: Number of bytes to be copied.
+ * Description: copies n bytes of the src string
+ *
+ * Return: Return dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int x = 0;
+
+	while (x < n)
+	{
+		dest[x] = src[x];
+
+		if (*(src + x) == '\0')
+			break;
+		x++;
+	}
+
+	*(dest + x) = '\0';
+
+	return (dest);
+}
