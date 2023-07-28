5240dff5b423fca8491a96559266b497993b1cf1
Emmanuel <emmanuelonyekachi04122000@gmail.com>
2023-07-28 23:05:46 +0000

    Task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -13,13 +13,14 @@
 char *leet(char *s)
 {
 	char *ptr = s;
-	char s1[] = "aAeEoOtTlL";
-	char s2[] = "4433003311";
+	const char s1[] = "aAeEoOtTlL";
+	const char s2[] = "4433003311";
 	int j;
+	int size = (sizeof(s1)/sizeof(char) - 1);
 
 	while (*s)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = 0; j < size; j++)
 		{
 			if (*s == *(s1 + j))
 				*s = *(s2 + j);
