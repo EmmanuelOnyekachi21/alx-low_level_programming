23adb86492d0d7867779e44073d295e986766514
Emmanuel <emmanuelonyekachi04122000@gmail.com>
2023-08-09 14:40:36 +0000

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: prints name of program followed by a new line
+ *
+ * Return: Return 0 if successful
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
