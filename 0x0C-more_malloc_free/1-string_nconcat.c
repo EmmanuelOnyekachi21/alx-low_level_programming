23481ab00a2a4677b46bca125d0021918f732b8d
Emmanuel <emmanuelonyekachi04122000@gmail.com>
2023-08-12 01:31:39 +0000

    Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -18,16 +18,16 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int length_1 = 0, length_2 = 0, i;
 	char *ptr;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
 	while (s1[length_1])
 		length_1++;
 
 	while (s2[length_2])
 		length_2++;
-
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
 
 	ptr = malloc(sizeof(char) * (length_1 + n + 1));
 
