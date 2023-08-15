2e2d7189fd8660269b40813542a6a184517e8fc5
Emmanuel <emmanuelonyekachi04122000@gmail.com>
2023-08-15 20:26:55 +0000

    dog heder

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+ * struct dog - Dog attributes
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: The Dog's owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+
+#endif
