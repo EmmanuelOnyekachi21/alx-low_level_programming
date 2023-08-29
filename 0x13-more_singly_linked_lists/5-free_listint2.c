2a2cd3d2824b539a0b225b8f5f462c59d42f5fbe
Emmanuel <emmanuelonyekachi04122000@gmail.com>
2023-08-29 01:06:34 -0700

    Task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * free_listint2 - Entry point
+ * @head: Head
+ * Description: frees a listint_t list.
+ *
+ * Return: Return nothing
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head)
+	{
+		temp = *head;
+		(*head) = (*head)->next;
+		free(temp);
+	}
+	(*head) = NULL;
+}
