751b7e09bf8b02e629d06b54f6167f976c56ade5
Emmanuel <emmanuelonyekachi04122000@gmail.com>
2023-08-30 05:40:24 -0700

    Task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,42 +1,30 @@
-#include <stdio.h>
 #include "main.h"
-/**
- * _sqrt_recursion - Entry point
- *
- * @n: Number whose natural root is to be found
- *
- * Description: returns the natural square root of a number.
- *
- * Return: The square root of the number, or -1 if no natural square root.
- */
-
 int check_sqrt(int n, int guess);
 
+/**
+ * check_sqrt_recursion - It returns the value of square guess of n.
+ * @n: an input integer
+ * Return: The square guess of n
+ */
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	if (n == 0 || n == 1)
-		return (n);
 
-	return (check_sqrt(n, n/2));
+	return (check_sqrt(1, n));
 }
 
 /**
- * check_sqrt - Helper function to recursively find square root.
- * @n: The number for which to find the square root.
- * @guess: The current guess for the square root
- *
- * Return: The square root or -1 if not found
+ * check_sqrt - find square guess
+ * @n: nious value
+ * @guess: square guess value
+ * Return: the square guess
  */
-
 int check_sqrt(int n, int guess)
 {
-	if (guess * guess == n)
-		return (guess);
-
-	if (guess <= 0)
+	if (n > guess)
 		return (-1);
-
-	return (check_sqrt(n, guess - 1));
+	else if (n * n == guess)
+		return (n);
+	return (check_sqrt(n + 1, guess));
 }
