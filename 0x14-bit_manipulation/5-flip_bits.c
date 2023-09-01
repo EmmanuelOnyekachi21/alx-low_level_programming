e6f3b6f868297ed4b7612f9aa9acbe13b91c4a18
Emmanuel <emmanuelonyekachi04122000@gmail.com>
2023-09-01 13:40:25 -0700

    Task 5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -5,7 +5,7 @@
  *
  * @n: The first unsigned long int number.
  * @m: The second unsigned long int number.
- * Description: Returns the number of bits to flip to convert one number to another.
+ * Description: Returns the number of bits 2 flip 2 convert a number to another
  * Return: The number of bits that need to be flipped to convert n to m.
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
