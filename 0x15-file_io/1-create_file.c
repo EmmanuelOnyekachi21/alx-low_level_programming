549b3b32f2ff4fb3f518b86a44e500a1c18acbbf
Emmanuel Onyekachi <emmanuelonyekachi04122000@gmail.com>
2023-10-14 21:11:13 +0000

    TASK ONE

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		text_content = "";
 
-	while (text_content[length++])
+	while (text_content[++length])
 		;
 
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
