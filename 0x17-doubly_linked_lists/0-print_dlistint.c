4b2fcbedd6b84a23688d9107c45c0fa417e4231b
Demon <emmanuelonyekachi04122000@gmail.com>
2023-12-13 17:55:02 +0000

    Task 0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_dlistint - Entry point
+ *
+ * Description: prints all the elements of a dlistint_t list.
+ * @h: head
+ *
+ * Return: the number of nodes.
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *current = NULL;
+	size_t nodes = 0;
+	current = h;
+
+	while (current)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+		nodes++;
+	}
+
+	return (nodes);
+}
